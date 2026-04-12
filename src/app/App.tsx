5507d54b7d4320a86a35aa26859652e17df08022
Shubh Kapoor <shubhkapoor0904@gmail.com>
2026-04-12 19:54:48 +0530

    test navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,9 @@
+import Navbar from "./components/Navbar";
 export default function App() {
-  return <h1>WORKING</h1>;
+  return (
+    <>
+      <h1>WORKING</h1>
+      <Navbar />
+    </>
+  );
 }
