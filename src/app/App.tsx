044c86a673e099e057e4e40c4b6988e9e092596c
Shubh Kapoor <shubhkapoor0904@gmail.com>
2026-04-12 20:08:19 +0530

    stable version after fixes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,31 @@
-import Navbar from "./components/Navbar";
+import { Navbar } from "./components/Navbar";
+import { Hero } from "./components/Hero";
+import { StorySection } from "./components/StorySection";
+import { BrandLogos } from "./components/BrandLogos";
+import { Services } from "./components/Services";
+import { Testimonial } from "./components/Testimonial";
+import { CTA } from "./components/CTA";
+import { Footer } from "./components/Footer";
+import { InstagramFeed } from "./components/InstagramFeed";
+import { CustomCursor } from "./components/ui/CustomCursor";
+import { Toaster } from "sonner";
+
 export default function App() {
   return (
-    <>
-      <h1>WORKING</h1>
+    <div className="min-h-screen bg-black">
+      <CustomCursor />
       <Navbar />
-    </>
+      <main>
+        <Hero />
+        <StorySection />
+        <BrandLogos />
+        <Services />
+        <Testimonial />
+        <CTA />
+        <InstagramFeed />
+      </main>
+      <Footer />
+      <Toaster theme="dark" position="bottom-right" />
+    </div>
   );
 }
