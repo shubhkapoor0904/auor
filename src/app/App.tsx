8e224c9ba6f6f70b2f0d5517c2ecb376a581238d
Shubh Kapoor <shubhkapoor0904@gmail.com>
2026-04-12 19:39:48 +0530

    isolate components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,14 @@
 export default function App() {
-  return <h1>WORKING</h1>;
+  return (
+    <>
+      <h1>WORKING</h1>
+
+      {/* Add components ONE BY ONE */}
+
+      {/* <InstagramFeed /> */}
+      {/* <Hero /> */}
+      {/* <Navbar /> */}
+
+    </>
+  );
 }
